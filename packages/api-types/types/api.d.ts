839814bc78c22f9fe4ec28f173bb4f57b253a84e
Raminder Singh <romi_ssk@yahoo.co.in>
2024-10-15 15:36:36 +0530

    create source when enable replication button is clicked

--- packages/api-types/types/api.d.ts
+++ packages/api-types/types/api.d.ts
@@ -1031,6 +1031,8 @@ export interface paths {
   '/platform/replication/{ref}/sources': {
     /** Gets replication sources */
     get: operations['ReplicationSourcesController_getSources']
+    /** Creates a replication source */
+    post: operations['ReplicationSourcesController_createSource']
   }
   '/platform/reset-password': {
     /** Reset password for email */
@@ -3171,6 +3173,9 @@ export interface components {
        */
       name: string
       value: string
+    }
+    CreateSourceResponse: {
+      id: number
     }
     CreateStorageBucketBody: {
       allowed_mime_types: string[]
@@ -13920,6 +13925,26 @@ export interface operations {
         }
       }
       /** @description Failed to get replication sources */
+      500: {
+        content: never
+      }
+    }
+  }
+  /** Creates a replication source */
+  ReplicationSourcesController_createSource: {
+    parameters: {
+      path: {
+        /** @description Project ref */
+        ref: string
+      }
+    }
+    responses: {
+      201: {
+        content: {
+          'application/json': components['schemas']['CreateSourceResponse']
+        }
+      }
+      /** @description Failed to create replication source */
       500: {
         content: never
       }
